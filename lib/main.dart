38cf831f3334b7ae28c86f05e5187a913c92b2c7
OhioLight <142236532+OhioLight@users.noreply.github.com>
2023-12-20 11:13:18 +0100

    background

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,49 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        drawer: SafeArea(
+          child: Drawer(
+            width: MediaQuery.of(context).size.width - 60,
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/illustrations/candy_BG.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 0, 50),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      ClipOval(
+                        child: SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: Image(
+                            image: AssetImage('assets/illustrations/kai.png'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: Stack(
+          children: [
+            Positioned.fill(
+              child: Image.asset(
+                'assets/illustrations/branch_BG.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
         ),
       ),
     );
