eb3b0a2f55b5f942789d88285e9bf2a9cab3efc4
OhioLight <142236532+OhioLight@users.noreply.github.com>
2023-12-21 16:50:07 +0100

    log out text zu textbutton gemacht

--- lib/Drawer/widgets/list_view_drawer.dart
+++ lib/Drawer/widgets/list_view_drawer.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:weihnachtsspecial/Drawer/widgets/drawer_button.dart';
 import 'package:weihnachtsspecial/Drawer/widgets/drawer_text.dart';
+import 'package:weihnachtsspecial/Pages/login_page.dart';
 
 class ListViewDraw extends StatelessWidget {
   const ListViewDraw({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(
+        const SizedBox(
           height: 15,
         ),
-        ListTile(
+        const ListTile(
             contentPadding: EdgeInsets.fromLTRB(20, 20, 0, 0),
             title: DrawerText()),
-        SizedBox(
+        const SizedBox(
           height: 30,
         ),
-        ListTile(
+        const ListTile(
           contentPadding: EdgeInsets.fromLTRB(0, 20, 0, 0),
           title: DrawerBtn(
             color1: Color.fromARGB(255, 173, 203, 230),
@@ -28,7 +29,7 @@ class ListViewDraw extends StatelessWidget {
             buttonimage: 'assets/illustrations/house.png',
           ),
         ),
-        ListTile(
+        const ListTile(
             contentPadding: EdgeInsets.fromLTRB(0, 20, 0, 0),
             title: DrawerBtn(
               color1: Color.fromARGB(255, 140, 206, 177),
@@ -36,7 +37,7 @@ class ListViewDraw extends StatelessWidget {
               buttontext: 'Gifts',
               buttonimage: 'assets/illustrations/gift.png',
             )),
-        ListTile(
+        const ListTile(
             contentPadding: EdgeInsets.fromLTRB(0, 20, 0, 0),
             title: DrawerBtn(
               color1: Color.fromARGB(255, 255, 148, 142),
@@ -44,7 +45,7 @@ class ListViewDraw extends StatelessWidget {
               buttontext: 'Order',
               buttonimage: 'assets/illustrations/stars.png',
             )),
-        ListTile(
+        const ListTile(
             contentPadding: EdgeInsets.fromLTRB(0, 20, 0, 0),
             title: DrawerBtn(
               color1: Color.fromARGB(255, 250, 214, 74),
@@ -52,16 +53,25 @@ class ListViewDraw extends StatelessWidget {
               buttontext: 'Settings',
               buttonimage: 'assets/illustrations/ bauble.png',
             )),
-        Spacer(),
+        const Spacer(),
         Padding(
-          padding: EdgeInsets.fromLTRB(25, 0, 0, 20),
-          child: Text(
-            'Log out',
-            style: TextStyle(
-                fontSize: 18,
-                fontFamily: 'SF',
-                color: Color.fromARGB(255, 134, 134, 134),
-                fontWeight: FontWeight.w600),
+          padding: const EdgeInsets.fromLTRB(25, 0, 0, 20),
+          child: TextButton(
+            child: const Text(
+              'Log out',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'SF',
+                  color: Color.fromARGB(255, 134, 134, 134),
+                  fontWeight: FontWeight.w600),
+            ),
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ));
+            },
           ),
         ),
       ],
